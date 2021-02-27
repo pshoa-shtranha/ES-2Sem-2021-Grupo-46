939a010dfcea7bbc4b4a6a1c22a6332dfee2c6a6
lalss <lalss@iscte-iul.pt>
2021-02-27 12:19:31 +0000

    update3

--- ProjetoES/src/ES1/main.java
+++ ProjetoES/src/ES1/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!"); //helloooo
+		System.out.println("Hello world!"); //helloooog
 		//asdfh
 	}
 
