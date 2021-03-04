1d11918e08428774af904db04c74d56b13c5ef85
mbcca <mbcca@iscte-iul.pt>
2021-03-04 22:20:57 +0000

    okok

---
+++ ProjetoES/src/ES1/prática2.java
@@ -0,0 +1,11 @@
+package ES1;
+
+public class prática2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		//asdfh
+	}
+
+}
