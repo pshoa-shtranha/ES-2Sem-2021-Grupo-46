6ec41a244f758fab31796acb48c6c19d09562657
pshoa-shtranha <pshoa@iscte-iul.pt>
2021-03-13 21:06:10 +0000

    criação de projeto

---
+++ es-g46/src/test/java/es_g46/es_g46/AppTest.java
@@ -0,0 +1,20 @@
+package es_g46.es_g46;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
