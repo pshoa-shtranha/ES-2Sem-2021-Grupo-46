587d841db5fd0a0612d61bbc91f7f2546b57b8d9
mbcca <mbcca@iscte-iul.pt>
2021-03-14 18:46:22 +0000

    gui

--- es-g46/src/main/java/es_g46/es_g46/GUI.java
+++ es-g46/src/main/java/es_g46/es_g46/GUI.java
@@ -2,17 +2,25 @@ package es_g46.es_g46;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class GUI {
 	
-	private static JFrame frame;
-	private static JTextField text;
-	private static JButton button;
+	public static  JFrame frame;
+	private  JTextField text;
 
+
+	public GUI() {
+		frame = new JFrame("ES");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		addFrameContent();
+		frame.pack();
+	}
 	
-	public static void open() {
+	public void open() {
 		frame.setLocation(100, 50);
 		frame.setSize(1000, 500);
 		frame.setVisible(true);
@@ -20,13 +28,20 @@ public class GUI {
 	
 	private void addFrameContent() {
 		frame.setLayout(new BorderLayout());
+		
+		JButton button = new JButton("Extrair métricas"); 
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//colocar ação do botão
+				
+			}
+		});
+		frame.add(button, BorderLayout.SOUTH);
 	}
 	
 	
 	public static void main(String args[]) {
-		frame = new JFrame("ES");
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.pack();
-		open();
+		GUI gui = new GUI();
+		gui.open();
 	}
 }
