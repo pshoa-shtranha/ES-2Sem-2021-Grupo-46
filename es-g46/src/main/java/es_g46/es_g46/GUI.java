14337d196db4597f046e949f22d7fa9c41e6609e
mbcca <mbcca@iscte-iul.pt>
2021-03-18 21:34:56 +0000

    GUI inicial

--- es-g46/src/main/java/es_g46/es_g46/GUI.java
+++ es-g46/src/main/java/es_g46/es_g46/GUI.java
@@ -6,6 +6,7 @@ import  org.apache.poi.hssf.usermodel.HSSFSheet;
 import  org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import  org.apache.poi.hssf.usermodel.HSSFRow;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,15 +15,12 @@ import javax.swing.*;
 public class GUI {
 	
 	private JFrame frame;
-	private JButton button;
-	private JButton button1;
-	private String text;
 	private JPanel topPanel;
 	private JPanel bottomPanel;
 
 
 	public GUI() {
-		frame = new JFrame("ES");
+		frame = new JFrame("Excel Reader");
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		addFrameContent();
 		frame.pack();
@@ -62,51 +60,96 @@ public class GUI {
 	
 	private void upPanel() {
 		
-		button = new JButton("Open Excel"); 
+		JButton button = new JButton("Show Excel"); 
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//colocar ação do botão
-				//createExcel();
 			}
 		});
-		frame.add(button, BorderLayout.SOUTH);
 		
-		button1 = new JButton("Open Excellll"); 
+		JButton button1 = new JButton("Reset Excel"); 
 		button1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//colocar ação do botão
-				//createExcel();
+			}
+		});
+		
+		JButton button2 = new JButton("Add Rule"); 
+		button2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//colocar ação do botão
+			}
+		});
+		
+		JButton button3 = new JButton("Delete all Rules"); 
+		button3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//colocar ação do botão
+			}
+		});
+		
+		JButton button4 = new JButton("Detect Code Smells"); 
+		button4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//colocar ação do botão
+			}
+		});
+		
+		JButton button5 = new JButton("Evaluate Quality"); 
+		button5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//colocar ação do botão
+			}
+		});
+		
+		JButton button6 = new JButton("Existing Rules"); 
+		button6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//colocar ação do botão
+			}
+		});
+		
+		JButton button7 = new JButton("Data Size"); 
+		button7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//colocar ação do botão
+			}
+		});
+		
+		JButton button8 = new JButton("Exit"); 
+		button8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//colocar ação do botão
 			}
 		});
 		
 		topPanel = new JPanel();
 		frame.add(topPanel, BorderLayout.SOUTH);
 		topPanel.setLayout(new FlowLayout());
+		
 		topPanel.add(button);
 		topPanel.add(button1);
+		topPanel.add(button2);
+		topPanel.add(button3);
+		topPanel.add(button4);
+		topPanel.add(button5);
+		topPanel.add(button6);
+		topPanel.add(button7);
+		topPanel.add(button8);
 	}
 	
-/*	private void lowPanel() {
-		
-		button = new JButton("Open Excel"); 
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//colocar ação do botão
-			}
-		});
-		frame.add(button, BorderLayout.SOUTH);
-		
+	private void lowPanel() {
+	
 		bottomPanel = new JPanel();
 		frame.add(bottomPanel, BorderLayout.NORTH);
-		bottomPanel.setLayout(new FlowLayout());
-		topPanel.add(button);
+		bottomPanel.setLayout(new GridLayout(1,5));
 	}
-*/	
+	
 	
 	private void addFrameContent() {
 		frame.setLayout(new BorderLayout());
 		upPanel();
-//		lowPanel();
+		lowPanel();
 	}
 	
 	
