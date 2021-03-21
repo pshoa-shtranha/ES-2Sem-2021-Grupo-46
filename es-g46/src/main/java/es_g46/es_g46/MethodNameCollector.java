60389f78ee8f639e79c8c473ffe2af8c21c245a4
lalss <lalss@iscte-iul.pt>
2021-03-21 12:58:34 +0000

    MethodName - Version 1

---
+++ es-g46/src/main/java/es_g46/es_g46/MethodNameCollector.java
@@ -0,0 +1,16 @@
+package es_g46.es_g46;
+
+import java.util.List;
+
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+public class MethodNameCollector extends VoidVisitorAdapter<List<String>> {
+	
+	@Override
+	 public void visit(MethodDeclaration md, List<String> collector) {
+	 super.visit(md, collector);
+	 collector.add(md.getNameAsString());
+	 }
+
+}
