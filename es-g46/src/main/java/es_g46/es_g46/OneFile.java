28d906258f6c45211c88d62a310791a3a2e40202
lalss <lalss@iscte-iul.pt>
2021-03-21 16:48:49 +0000

    OneFile - version2

--- es-g46/src/main/java/es_g46/es_g46/OneFile.java
+++ es-g46/src/main/java/es_g46/es_g46/OneFile.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import com.github.javaparser.Range;
 import com.github.javaparser.StaticJavaParser;
@@ -48,6 +49,22 @@ public class OneFile extends Thread {
 
 	public void run() {
 		
+		String coluna1 = this.getMethodID();
+		System.out.println(coluna1);
+		board[numberFile - 1][0] = coluna1;
+		
+		String coluna2 = this.getPackageName();
+		System.out.println(coluna2);
+		board[numberFile - 1][1] = coluna2;
+		
+		String coluna3 = this.getClassName();
+		System.out.println(coluna3);
+		board[numberFile - 1][2] = coluna3;
+		
+		String coluna4 = this.getNameMethods();
+		board[numberFile - 1][3] = coluna4;
+		System.out.println(coluna4);
+		
 			for(int i = 0; i < id.length; i++) {
 				
 				if(id[i] == 1) {
@@ -56,54 +73,48 @@ public class OneFile extends Thread {
 					
 						case 0:
 							
-							String result = this.getNameMethods();
-							board[numberFile - 1][i] = result;
-							System.out.println(result);
-							//NOM_class
+							String coluna5 = this.getNumberMethods();
+							board[numberFile - 1][i + 2] = coluna5;
+							System.out.println(coluna5);
+							//nom_class
+							//numero metodos por classe
 							break;
 						case 1:
-							//cyclesClass();
-							//WMC_class
-							String result1 = this.getCyclicClass();
-							board[numberFile - 1][i] = result1;
-							System.out.println(result1);
+							String coluna6 = this.getClassSize();
+							board[numberFile - 1][i + 2] = coluna6;
+							System.out.println(coluna6);
+							//numero de linhas da classe
+							//loc_class
+							
 							break;
 						case 2:
-							String result2 = this.getSizeMethods();
-							board[numberFile - 1][i] = result2;
-							System.out.println(result2);
-							//LOC_method
+							
+							//complexidade ciclica da classe
+							String coluna7 = this.getCyclicClass();
+							board[numberFile - 1][i + 2] = coluna7;
+							System.out.println(coluna7);
 							break;
 						case 3:
-							String result3 = this.getCyclicMethods();
-							board[numberFile - 1][i] = result3;
-							System.out.println(result3);
+							String coluna8 = this.getSizeMethods();
+							board[numberFile - 1][i + 2] = coluna8;
+							System.out.println(coluna8);
+							//numero de linhas do metodo
 							
-							//CYCLO_method
 							break;
 						case 4:
-							//numberLinesClass();
-							//LOC_class
-							StringBuilder list = new StringBuilder();
-							list.append("|");
-							List<String> classSizes = new ArrayList<>();
-							VoidVisitor<List<String>> methodSizeVisitor = new ClassSize();
-							methodSizeVisitor.visit(this.cu, classSizes);
-							while(!classSizes.isEmpty()) {
-								String method = classSizes.remove(0);
-								
-								list.append(method);
-								System.out.println(method);
-								list.append("|");
-							}
-							System.out.println(list.toString());
+							
+							String coluna9 = this.getCyclicMethods();
+							board[numberFile - 1][i + 2] = coluna9;
+							System.out.println(coluna9);
+							
+							//complexidade ciclioca do metodo
 							break;
 					}
 				}
 			}
 	}
 	
-	public static void main(String[] args) throws InterruptedException {
+	/*public static void main(String[] args) throws InterruptedException {
 		
 		final String FILE_PATH = "C:\\Users\\Maintenant Prêt\\Desktop\\ES\\FileManagement.java";
 		int[] smells = new int[5];
@@ -112,14 +123,14 @@ public class OneFile extends Thread {
 		smells[2] = 1;
 		smells[3] = 1;
 		smells[4] = 1;
-		String[][] board = new String[1][smells.length];
+		String[][] board = new String[1][smells.length + 4];
 		File file = new File(FILE_PATH);
 		OneFile a = new OneFile(smells, file, board, 1);
 		a.start();
 		a.join();
-		System.out.println("O programa terminou!");
+		System.out.println("A pesquisa no ficheiro terminou!");
 
-	}
+	}*/
 	
 	public String getNameMethods() {
 		
@@ -133,7 +144,6 @@ public class OneFile extends Thread {
 			String method = methodNames.remove(0);
 			
 			list.append(method);
-			System.out.println(method);
 			list.append("|");
 		}
 		return list.toString();
@@ -151,7 +161,6 @@ public class OneFile extends Thread {
 			String method = methodSizes.remove(0);
 			
 			list.append(method);
-			System.out.println(method);
 			list.append("|");
 		}
 		return list.toString();
@@ -168,7 +177,6 @@ public class OneFile extends Thread {
 			String method = methodSizes.remove(0);
 			
 			list.append(method);
-			System.out.println(method);
 			list.append("|");
 		}
 		return list.toString();
@@ -187,12 +195,103 @@ public class OneFile extends Thread {
 			count = count + Integer.valueOf(method);
 			
 		}
-		System.out.println(count);
-		for(int i = 0; i < 2; i++) {
+		for(int i = 0; i < this.getTotalMethods(); i++) {
 			list.append(count);
 			list.append("|");
 		}
 		return list.toString();
 	}
 	
+	public String getMethodID() {
+		
+		StringBuilder list = new StringBuilder();
+		list.append("|");
+		List<String> methodNames = new ArrayList<>();
+		VoidVisitor<List<String>> methodNameVisitor = new MethodNameCollector();
+		methodNameVisitor.visit(this.cu, methodNames);
+		int count = 0;
+		while(!methodNames.isEmpty()) {
+			methodNames.remove(0);
+			
+			list.append(count + 1);
+			count++;
+			list.append("|");
+		}
+		return list.toString();
+	}
+	
+	public String getNumberMethods() {
+		
+		StringBuilder list = new StringBuilder();
+		list.append("|");
+		List<String> methodNames = new ArrayList<>();
+		VoidVisitor<List<String>> methodNameVisitor = new MethodNameCollector();
+		methodNameVisitor.visit(this.cu, methodNames);
+		int total = methodNames.size();
+		
+		while(!methodNames.isEmpty()) {
+			methodNames.remove(0);
+			list.append(total);
+			list.append("|");
+		}
+		
+		return list.toString();
+	}
+	
+	public int getTotalMethods() {
+		
+		List<String> methodNames = new ArrayList<>();
+		VoidVisitor<List<String>> methodNameVisitor = new MethodNameCollector();
+		methodNameVisitor.visit(this.cu, methodNames);
+		int total = methodNames.size();
+		return total;
+	}
+	
+	public String getClassSize() {
+		
+		StringBuilder list = new StringBuilder();
+		list.append("|");
+		List<String> classSizes = new ArrayList<>();
+		VoidVisitor<List<String>> methodSizeVisitor = new ClassSize();
+		methodSizeVisitor.visit(this.cu, classSizes);
+		int size = this.getTotalMethods();
+		String tamanho = classSizes.remove(0);
+		for(int i = 0; i < size; i++) {
+			list.append(tamanho);
+			list.append("|");
+		}
+		return list.toString();
+	}
+	
+	public String getClassName() {
+		
+		StringBuilder list = new StringBuilder();
+		list.append("|");
+		List<String> classSizes = new ArrayList<>();
+		VoidVisitor<List<String>> methodSizeVisitor = new ClassName();
+		methodSizeVisitor.visit(this.cu, classSizes);
+		int size = this.getTotalMethods();
+		String nome = classSizes.remove(0);
+		for(int i = 0; i < size; i++) {
+			list.append(nome);
+			list.append("|");
+		}
+		return list.toString();
+	}
+	
+	public String getPackageName() {
+		
+		Scanner reader;
+		String pacote = null;
+		try {
+			reader = new Scanner(this.files);
+			pacote = reader.nextLine().toString();
+			reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return pacote;
+	}
 }
