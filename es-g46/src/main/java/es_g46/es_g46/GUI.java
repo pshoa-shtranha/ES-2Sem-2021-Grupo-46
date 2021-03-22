3ed4298ab5b3df7fec7ce3f6da706c15617049cf
pshoa-shtranha <pshoa@iscte-iul.pt>
2021-03-22 16:43:58 +0000

    vizualizar excel + botão 6 da Margarida

--- es-g46/src/main/java/es_g46/es_g46/GUI.java
+++ es-g46/src/main/java/es_g46/es_g46/GUI.java
@@ -1,24 +1,33 @@
 package es_g46.es_g46;
 
 import java.awt.BorderLayout;
-import  java.io.*;
-import java.util.Scanner;
-import  org.apache.poi.hssf.usermodel.HSSFSheet;
-import  org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import  org.apache.poi.hssf.usermodel.HSSFRow;
 import java.awt.FlowLayout;
-import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
-import javax.swing.text.html.HTMLDocument.Iterator;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 import javax.swing.table.DefaultTableModel;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class GUI {
 	
@@ -29,6 +38,16 @@ public class GUI {
 	private File file;
 	private String path = "C:\\Users\\marga\\OneDrive\\Ambiente de Trabalho\\Code_Smells.xlsx";
 	private JTable table;
+	
+	private JCheckBox checkBox1;
+	private JCheckBox checkBox2;
+	private JCheckBox checkBox3;
+	private JCheckBox checkBox4;
+	private JCheckBox checkBox5;
+	private JTextField text_metricas;
+	
+	private int[] metricas;
+	private File[] list;
 	
 
 	public GUI() {
@@ -114,13 +133,95 @@ public class GUI {
 			}  
 	}
 	*/
+private int[] buttonMetricas() {
+		
+		metricas = new int[5];
+		metricas[0] = 0;
+		metricas[1] = 0;
+		metricas[2] = 0;
+		metricas[3] = 0;
+		metricas[4] = 0;
+		
+		
+		 for (int i=0; i<metricas.length; i++){
+         if (checkBox1.isSelected()) {
+        	metricas[0] = 1;
+         
+        	} if (checkBox2.isSelected()){
+        		metricas[1] = 1;
+        		
+        	} if (checkBox3.isSelected()) {
+        		metricas[2] = 1;
+        		
+        	} if (checkBox4.isSelected()) {
+        		metricas[3] = 1;
+        		
+        	} if (checkBox5.isSelected()) {
+        		metricas[4] = 1;
+        		
+        	}else {
+        	//	System.out.println("erro");
+        	}
+         System.out.println(metricas[i]);
+        }
+		 System.out.println(Arrays.toString(metricas));
+        return metricas;        
+        }
+
 	private void lowPanel() {
 		
-		JButton button = new JButton("Show Excel"); 
+		JButton button = new JButton("Import Excel"); 
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//colocar ação do botão
-				//showExcel();
+				File excelFile;
+				FileInputStream excelFIS = null;
+				BufferedInputStream excelBIS = null;
+				XSSFWorkbook excelJtableImport = null;
+				
+				String defaultCurrentDirectoryPath = "src/";
+				JFileChooser excelFileChooser = new JFileChooser(defaultCurrentDirectoryPath);
+				int excelChooser = excelFileChooser.showOpenDialog(null);
+				//if import button is clicked
+				if (excelChooser == JFileChooser.APPROVE_OPTION) {
+					try {
+						excelFile = excelFileChooser.getSelectedFile();
+						excelFIS = new FileInputStream(excelFile);
+						excelBIS = new BufferedInputStream(excelFIS);
+						
+						excelJtableImport = new XSSFWorkbook(excelBIS);
+						XSSFSheet excelSheet = excelJtableImport.getSheetAt(0);
+						
+						//Looping through excel columns and rows (começa na 2a fila)
+						for(int row = 1; row < excelSheet.getLastRowNum(); row++) {
+							XSSFRow excelRow = excelSheet.getRow(row);
+							
+							DefaultTableModel model = (DefaultTableModel) table.getModel();
+							model.addRow(new Object[] {excelRow.getCell(0), excelRow.getCell(1),
+									excelRow.getCell(2), excelRow.getCell(3), excelRow.getCell(4),
+									excelRow.getCell(5), excelRow.getCell(6), excelRow.getCell(7), excelRow.getCell(8), });
+							
+//							for(int column = 0; column < excelRow.getLastCellNum(); column++) {
+//								XSSFCell excelCell = excelRow.getCell(column);
+//								if(excelCell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC) {
+//									System.out.println(excelCell.getNumericCellValue());									
+//								}
+//								else if(excelCell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
+//									System.out.println(excelCell.getStringCellValue());
+//								}
+//							}
+						}
+						
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+						JOptionPane.showMessageDialog(null, e1.getMessage());
+					} catch (IOException e2) {
+						// TODO Auto-generated catch block
+						e2.printStackTrace();
+						JOptionPane.showMessageDialog(null, e2.getMessage());
+					}
+					
+				}
 			}
 		});
 		
@@ -159,12 +260,56 @@ public class GUI {
 			}
 		});
 		
-		JButton button6 = new JButton("Existing Rules"); 
+		
+		
+		JButton button6 = new JButton("Regras guardadas"); 
 		button6.addActionListener(new ActionListener() {
+			
 			public void actionPerformed(ActionEvent e) {
-				//colocar ação do botão
+				gui_metricas();
+				
+							
+				JLabel label_metricas = new JLabel("Where is the file?");
+				text_metricas = new JTextField(10);
+
+				JPanel frame2 = new JPanel(); 
+				frame_metricas.add(frame2, BorderLayout.NORTH);
+				frame2.setLayout(new FlowLayout() );
+				
+				frame2.add(label_metricas);
+				frame2.add(text_metricas);
+				
+				JPanel frame3 = new JPanel(); 
+				frame_metricas.add(frame3, BorderLayout.CENTER);
+				frame3.setLayout(new GridLayout(6,1));
+				
+				checkBox1 = new JCheckBox("NOM_class");  
+		        checkBox2 = new JCheckBox("LOC_class");
+		        checkBox3 = new JCheckBox("WMC_class");
+		        checkBox4 = new JCheckBox("LOC_method");
+		        checkBox5 = new JCheckBox("CYCLO_method");
+ 
+		        frame3.add(checkBox1);  
+		        frame3.add(checkBox2);  
+		        frame3.add(checkBox3);
+		        frame3.add(checkBox4);
+		        frame3.add(checkBox5);
+
+		        
+		        JButton button20 = new JButton("Check"); 
+				button20.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						
+						 buttonMetricas();
+						// listFiles();
+					}
+				});
+				
+				frame_metricas.add(button20, BorderLayout.SOUTH);			
 			}
 		});
+		
+		
 /*		
 		JButton button7 = new JButton("Data Size"); 
 		button7.addActionListener(new ActionListener() {
@@ -261,15 +406,17 @@ public class GUI {
 		table = new JTable();
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
-				{"MethodID", "package", "class", "method", "NOM_class", "LOC_class", "WMC_class", "LOC_method", "CYCLO_Method"},
 			},
 			new String[] {
 				"MethodID", "package", "class", "method", "NOM_class", "LOC_class", "WMC_class", "LOC_method", "CYCLO_Method"
 			}
 		));
-		table.getColumnModel().getColumn(0).setResizable(false);
 		table.getColumnModel().getColumn(8).setPreferredWidth(91);
 		topPanel.add(table);
+		
+		JScrollPane js=new JScrollPane(table);
+		topPanel.add(js);
+//		js.setVisible(true);
 	}
 	
 	
